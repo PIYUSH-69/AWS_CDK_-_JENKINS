236d90593ae2c826467327fadbbf56cac4dcd13e
PIYUSH-69 <dalvi.piyu@gmail.com>
2026-02-02 22:35:07 +0530

    Initial commit

---
+++ test/jenkins_cicd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JenkinsCicd from '../lib/jenkins_cicd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jenkins_cicd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JenkinsCicd.JenkinsCicdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
