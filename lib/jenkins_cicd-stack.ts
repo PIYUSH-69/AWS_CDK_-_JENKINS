1f9d44fbceb077a019696077bbe14d3f4d1b8885
PIYUSH-69 <dalvi.piyu@gmail.com>
2026-02-12 16:14:34 +0530

    FEATURE: Addition of EC2 instance

--- lib/jenkins_cicd-stack.ts
+++ lib/jenkins_cicd-stack.ts
@@ -37,34 +37,34 @@ def lambda_handler(event, context):
     });
 
 
-         const vpc = new ec2.Vpc(this, 'Vpc', {
-      maxAzs: 2,
-      natGateways: 0,
-      subnetConfiguration: [
-        { name: 'public', subnetType: ec2.SubnetType.PUBLIC },
-        { name: 'private', subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
-      ],
-    });
+//       const vpc = new ec2.Vpc(this, 'Vpc', {
+//       maxAzs: 2,
+//       natGateways: 0,
+//       subnetConfiguration: [
+//         { name: 'public', subnetType: ec2.SubnetType.PUBLIC },
+//         { name: 'private', subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+//       ],
+//     });
 
-    /** ---------------------------
-     *  Single EC2 Instance
-     *  --------------------------*/
+//     /** ---------------------------
+//      *  Single EC2 Instance
+//      *  --------------------------*/
    
-const ec2Instance = new ec2.Instance(this, 'WebServer', {
-  vpc,
-  vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
-  instanceType: new ec2.InstanceType('t3.micro'),
-  machineImage: ec2.MachineImage.latestAmazonLinux2023(),
-  blockDevices: [
-    {
-      deviceName: '/dev/xvda',
-      volume: ec2.BlockDeviceVolume.ebs(8, {
-        deleteOnTermination: true,
-        encrypted: true,
-      }),
-    },
-  ],
-});
+// const ec2Instance = new ec2.Instance(this, 'WebServer', {
+//   vpc,
+//   vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+//   instanceType: new ec2.InstanceType('t3.micro'),
+//   machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+//   blockDevices: [
+//     {
+//       deviceName: '/dev/xvda',
+//       volume: ec2.BlockDeviceVolume.ebs(8, {
+//         deleteOnTermination: true,
+//         encrypted: true,
+//       }),
+//     },
+//   ],
+// });
 
     // If later your Lambda needs S3 access, uncomment:
     // bucket.grantReadWrite(fn);
